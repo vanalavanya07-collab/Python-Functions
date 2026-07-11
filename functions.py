2e5a864668628db16f52dc9a3893a6c56244fb4e
vanalavanya07-collab <vanalavanya07@gmail.com>
2026-07-11 23:15:55 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,22 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def even_odd(n):
+    if n % 2 == 0:
+        return "Even"
+    return "Odd"
+
+def square(n):
+    return n * n
+
+print(add(10, 20))
+print(subtract(30, 10))
+print(multiply(5, 4))
+print(even_odd(7))
+print(square(6))
